481a1abf1949ab528cca088d766406c7ca7fcaa4
Jason Snape <jasonsgithub@gmail.com>
2023-11-12 14:25:44 +0100

    rename of VPC and subnets

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -1,16 +1,16 @@
-resource "aws_vpc" "jason_vpc" {
+resource "aws_vpc" "vpc" {
   cidr_block = var.cidr_block
   enable_dns_support = true # default is true
   enable_dns_hostnames = false # default is false
 }
 
 resource "aws_subnet" "subnet_1" {
-  vpc_id     = aws_vpc.jason_vpc.id
+  vpc_id     = aws_vpc.vpc.id
   cidr_block = var.subnet_1_cidr_block
 }
 
 resource "aws_subnet" "subnet_2" {
-  vpc_id     = aws_vpc.jason_vpc.id
+  vpc_id     = aws_vpc.vpc.id
   cidr_block = var.subnet_2_cidr_block
 }
 
