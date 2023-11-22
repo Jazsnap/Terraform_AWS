d91f28c6d3d4ec0f737d33946222b0a580750e3e
Jason Snape <jasonsgithub@gmail.com>
2023-11-22 09:51:39 +0100

    comments + old subnet declarations removed

--- locals.tf
+++ locals.tf
@@ -3,6 +3,5 @@ locals {
     Environment = "dev"
     Project     = "Jason"
   }
-  subnet_prefixes = ["10.0.1", "10.0.2", "10.0.3"]
   number_of_subnets = 3
 }
--- main.tf
+++ main.tf
@@ -12,20 +12,6 @@ module "vpc" {
                   Name       = "subnet${idx + 1}"
                 }
   }
-  # subnets = {
-  #   "subnet1" = {
-  #     cidr_block = "10.0.1.0/24",
-  #     Name       = "subnet1"
-  #   },
-  #   "subnet2" = {
-  #     cidr_block = "10.0.2.0/24",
-  #     Name       = "subnet2"
-  #   },
-  #   "subnet3" = {
-  #     cidr_block = "10.0.3.0/24",
-  #     Name       = "subnet3"
-  #   }
-  # }
 }
 
 module "ec2_instance_1" {
